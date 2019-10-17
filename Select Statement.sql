3a1dd8961886e1a96658c112c1c3b6dd7974233b
Ethan-Chung <55211057+Ethan-Chung@users.noreply.github.com>
2019-10-17 13:37:39 -0700

    Add files via upload

--- Select Statement.sql
+++ Select Statement.sql
@@ -1,3 +1,6 @@
+/* Selects all data in the Tables Vendors */
 Select * from ap.dbo.vendors
+/* Finds all the data in the Column Vendorcity that has Washington in it */
 Where Vendorcity = 'Washington'
+/* Order all the data by VendorID in decending order */
 Order By VendorID desc;
